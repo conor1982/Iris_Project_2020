68c8ed27906830b032d5143a12bc2a094a7e1627
Conor O'Riordan <oriordanc@office.local>
2020-04-15 20:59:20 +0100

    commnd line argument added

--- iris_datachecks.py
+++ iris_datachecks.py
@@ -1,37 +1,37 @@
 #Conor O'Riordan
-#Past2 project plan
+#Part2 of project plan
 #Upload iris data
-#Data Checks
-#Explore Python
+#Data Sense Checks
+#Explore Pandas functions
 
 #Pandas libary import
 import pandas as pd
+import sys
 
-
-
+#ref https://pandas.pydata.org/pandas-docs/stable/reference/frame.html
+#ref https://www.shanelynn.ie/summarising-aggregation-and-grouping-data-in-python-pandas/
 #ref https://www.shanelynn.ie/using-pandas-dataframe-creating-editing-viewing-data-in-python/
 
 #iris data downloaded from http://archive.ics.uci.edu/ml/machine-learning-databases/iris/
 
 #import irisdata
-#command line prompt??
 #header = None added to not have first row as heading
-df = pd.read_csv('irisdata.csv', header=None)
+#df = pd.read_csv('irisdata.csv', header=None)
 
-#print first 5 lines check for headings
-print(df.head(5))
+#es.py weekly task
+df = pd.read_csv(f'{sys.argv[1]}', header=None)
 
 #Adding column names
-df.columns = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'Species']
+df.columns = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species']
 
 # \n to insert line bewteen print spaces
 
 #first 5 lines of dataset
-print("\nFirst 5 lines")
+print("\nFirst 5 lines of Dataset")
 print(df.head(5))
 
 #last 5 lines of data set
-print("\nLast 5 lines")
+print("\nLast 5 lines of Dataset")
 print(df.tail(5))
 
 #shape of data
@@ -39,17 +39,20 @@ print("\nShape of dataset Rows:Columns")
 print(df.shape)
 
 #dimension of data
-print("\nDatset dimensions")
+print("\nDataset Dimensions")
 print(df.ndim)
 
 #data types float string etc
-print("\nData types")
+print("\nData Types")
 print(df.dtypes)
 
-#Summary Stats of Dataset
-print("\nStats Summary")
-print(df.describe())
+#Count of variants
+print("\nCount of Iris Variants")
+print(df['species'].value_counts())
 
-print("Iris variant count")
-print(df['Species'].value_counts())
+#check for null values
+#ref https://stackoverflow.com/questions/26266362/how-to-count-the-nan-values-in-a-column-in-pandas-dataframe
+print("\nCount of Null values")
+print(df.isnull().sum())
+
 
