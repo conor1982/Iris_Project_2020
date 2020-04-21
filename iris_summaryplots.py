18d502d69ed8206bb2dea34d2e79891644871645
Conor O'Riordan <oriordanc@office.local>
2020-04-21 13:27:51 +0100

    Plot completion

--- iris_summaryplots.py
+++ iris_summaryplots.py
@@ -16,33 +16,65 @@ df = pd.read_csv('irisdata.csv', header=None)
 #Adding column names
 df.columns = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species']
 
-#pairplot
-sns.set_style("whitegrid");
-sns.pairplot(df,hue="species",height=4);
-plt.savefig("Pairplot")
-
-#Hist Function
-
-def makehist(x):
-    sns.FacetGrid(df,hue="species",height=6) \
-        .map(sns.distplot,x) \
-        .add_legend()
-    plt.title(x)
-    plt.savefig(x)
-
-#plot function
-def makeplot(x,y):
-    sns.FacetGrid(df,hue='species', height=4)\
-        .map(plt.scatter,x,y)\
-        .add_legend()
-    plt.show()
 #ref https://seaborn.pydata.org/tutorial/axis_grids.html
 #ref https://medium.com/@harimittapalli/exploratory-data-analysis-iris-dataset-9920ea439a3e
 #ref https://www.kaggle.com/rakesh6184/seaborn-plot-to-visualize-iris-data
 
-#heatmap
-plt.figure(figsize=(10,11))
-sns.heatmap(df.corr(),annot=True)
-plt.plot()
-plt.show()
 
+#PAIRPLOTS
+#ref https://seaborn.pydata.org/generated/seaborn.pairplot.html
+#pairplot of all
+
+#setting tickmarks and color palatte
+sns.set(style = 'ticks', color_codes=True)
+#colors for each species type
+sns.pairplot(df,hue = 'species')
+#save plot name
+plt.savefig("Pairplot of Iris Data")
+
+#pairplot petal width and petal length
+sns.set(style = 'ticks', color_codes=True)
+#plot just returning a pairplot for petal width and petal length
+sns.pairplot(df,hue = 'species',vars=['petal_width','petal_length'] )
+plt.savefig("Pairplot of Petal Width and Petal Length")
+
+#heatmap
+#ref https://seaborn.pydata.org/generated/seaborn.heatmap.html
+plt.figure(figsize=(10,11))
+#ref https://seaborn.pydata.org/tutorial/color_palettes.html
+#heatmap for the df, labels on, line widths adjusted and color palette chosen
+sns.heatmap(df.corr(),annot=True, linewidths=.5, cmap='Blues')
+#save map as heatmap.png
+plt.savefig('Heat Map')
+
+#Hist Function
+def makehist(x):
+    hist = sns.FacetGrid(df,hue="species",height=6) \
+        .map(sns.distplot,x) \
+        .add_legend()
+    #plt.title(x)
+    plt.savefig("Histogram of {}".format(x))
+    plt.close()
+    return hist
+
+#Scatterplot function
+def makeplot(x,y):
+    scatter = sns.FacetGrid(df,hue='species', height=4)\
+        .map(plt.scatter,x,y)\
+        .add_legend()
+    plt.savefig("Scatterplot of {} and {}".format(x,y))
+    plt.close()
+    return scatter
+
+
+#histogram function calls
+sep_len_hist = makehist('sepal_length')
+sep_width_hist = makehist('sepal_width')
+pet_len_hist = makehist('petal_length')
+pet_width_hist =makehist('petal_width')
+
+#scaterplot function calls
+scatter_sl_sw = makeplot('sepal_length','sepal_width')
+scatter_pl_pw = makeplot('petal_length','petal_width')
+scatter_sl_pw = makeplot('sepal_length','petal_width')
+scatter_pl_sw = makeplot('petal_length','sepal_width')
